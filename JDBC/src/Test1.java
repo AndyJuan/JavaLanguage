557a8c6a01abaf34fc7a970fda3a4bea5fdfd935
刘娟 <1937331536@qq.com>
2019-05-24 19:35:42 +0800

    no message

--- JDBC/src/Test1.java
+++ JDBC/src/Test1.java
@@ -10,7 +10,24 @@ import java.sql.SQLException;
 
 public class Test1 {
     public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("");
+       try{
+           Class.forName("com.mysql.jdbc.Driver");
+           System.out.println("数据库连接成功");
+           Connection c = DriverManager
+                   .getConnection(
+                           "jdbc:mysql://127.0.0.1:3306/howTEST?characterEncoding=UTF-8",
+                           "root", "admin123");
+
+           System.out.println("连接成功，获取连接对象： " + c);
+
+       } catch (ClassNotFoundException e) {
+           // TODO Auto-generated catch block
+           e.printStackTrace();
+       } catch (SQLException e) {
+           // TODO Auto-generated catch block
+           e.printStackTrace();
+       }
+
     }
 
 }
